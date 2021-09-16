c351652acbda269d2673994664abbf6850992c8c
NMagpie <sorochin.work@gmail.com>
2021-09-16 23:34:47 +0300

    Added audit file to test.

--- Lab1/src/com/Main.java
+++ Lab1/src/com/Main.java
@@ -1,8 +1,20 @@
 package com;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("portal_audits/Windows/MSCT_Windows_10_20H2_v1.0.0.audit");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (!line.matches("^#.*")){
+            line.split("<custom_item>");
+            System.out.println(line);}
+        }
+        scanner.close();
+
     }
 }
