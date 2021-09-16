2df504e6678758fe9a74dcd4fd5720af5cd0a082
PlotnicovPolina <plotnicov.polina@isa.utm.md>
2021-09-17 01:21:09 +0300

    First steps of parsing

--- Lab1/src/com/Main.java
+++ Lab1/src/com/Main.java
@@ -5,14 +5,32 @@ import java.util.*;
 
 public class Main {
 
+    HashMap<String,HashMap<String,String>> policies = new HashMap<>();
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("portal_audits/Windows/MSCT_Windows_10_20H2_v1.0.0.audit");
         Scanner scanner = new Scanner(file);
+        HashMap<String,String> policy;
+        String line;
+        String[] twoArguments= new String[] {};
         while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (!line.matches("^#.*")){
-            line.split("<custom_item>");
-            System.out.println(line);}
+            line = scanner.nextLine();
+                if (line.matches("\\s*<custom_item>\\s*")){
+                    line = scanner.nextLine();
+                    policy = new HashMap<>();
+                    while (!line.matches("\\s*</custom_item>\\s*"))
+                    {
+                    line=line.trim();
+                    twoArguments = line.split(" *:",2);
+                    if (twoArguments[0].matches("^\".*[^\"]$"))
+                        while (!line.matches(".*\"$"))
+                        {twoArguments[0]+=" "+line;
+                        line = scanner.nextLine();}
+                    policy.put(twoArguments[0],twoArguments[1]);
+                    line = scanner.nextLine();
+                    }
+                    System.out.println(policy);
+                }
         }
         scanner.close();
 
