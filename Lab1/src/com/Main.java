221409915688758a4258a0e1c7abcb4f5c220eb3
NMagpie <sorochin.work@gmail.com>
2021-09-17 03:26:03 +0300

    Slightly edited code

--- Lab1/src/com/Main.java
+++ Lab1/src/com/Main.java
@@ -6,38 +6,50 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
+
         File file = new File("portal_audits/Windows/MSCT_Windows_10_1507_v1.0.0.audit");
+
         Scanner scanner = new Scanner(file);
+
         HashMap<String,HashMap<String,String>> policies = new HashMap<>();
+
         HashMap<String,String> policy;
+
         String line;
+
         String[] twoArguments;
+
         while (scanner.hasNextLine()) {
             line = scanner.nextLine();
+
                 if (line.matches("\\s*<custom_item>\\s*")){
                     line = scanner.nextLine();
                     policy = new HashMap<>();
+
                     while (!line.matches("\\s*</custom_item>\\s*"))
                     {
                     line=line.trim();
                     twoArguments = line.split(" *: *",2);
+
                     if (twoArguments[1].matches("^\".*[^\"]$"))
                         while (!line.matches(".*\"$"))
                         {
                             line = scanner.nextLine();
                             twoArguments[1]+=" "+line;
                         }
+
                     policy.put(twoArguments[0],twoArguments[1]);
                     line = scanner.nextLine();
                     }
+
                     if ((policy.containsKey("reg_item"))&&(policy.containsKey("reg_key")))
                         policies.put(policy.get("reg_item").substring(1,policy.get("reg_item").length()-1)+"_"+policy.get("reg_key").substring(1,policy.get("reg_key").length()-1),policy);
                 }
         }
 
-        for (String key : policies.keySet()) {
+        for (String key : policies.keySet())
             System.out.println(key+" "+policies.get(key)+"\n");
-        }
+
         scanner.close();
 
     }
