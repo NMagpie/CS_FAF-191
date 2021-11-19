aaa83c1fd5530188faa8928757de3969ec758d6b
NMagpie <sorochin.work@gmail.com>
2021-11-19 13:09:59 +0200

    Added possibility to build executable JAR and start it using 'start.bat'

---
+++ SBT/src/main/java/main/Run.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class Run {
+
+    public static void main(String[] args) throws IOException, InvocationTargetException, IllegalAccessException {
+        Main.main(args);
+    }
+
+}
---
+++ start.bat
@@ -0,0 +1,1 @@
+java --add-opens java.prefs/java.util.prefs=ALL-UNNAMED -jar SBT-1.0-SNAPSHOT.jar
